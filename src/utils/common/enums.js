c489060121b4cc717f4a2c3e5e1a28b5a6b15922
DhruvKumar Nagar <iamdhruvnagar@gmail.com>
2023-06-29 23:38:48 +0530

    Added Booking model and utils folder from flight service

---
+++ src/utils/common/enums.js
@@ -0,0 +1,18 @@
+const SEAT_TYPE = {
+  ECONOMY: "economy",
+  PRIMIUM_ECONOMY: "primium_economy",
+  BUISNESS: "buisness",
+  FIRST_CLASS: "first_class",
+};
+
+const BOOKING_STATUS = {
+  BOOKED: "booked",
+  CANCELLED: "cancelled",
+  INITIATED: "initiated",
+  PENDING: "pending",
+};
+
+module.exports = {
+  SEAT_TYPE,
+  BOOKING_STATUS,
+};
